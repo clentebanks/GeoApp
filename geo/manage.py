b2bcda8ce803d5f543e90efed232dc1cdae9944f
clentebanks <clentebanks0@gmail.com>
2021-07-10 22:39:48 -0600

    dvds

--- geo/manage.py
+++ geo/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geo.settings','heroku config:set DISABLE_COLLECTSTATIC=1')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
